f62bf5a2f91fb7a8400af227ec209d90fcf51f8c
Prigozhaeva-Anastasia <nastaprigozaeva20@gmail.com>
2023-11-20 02:02:07 +0300

    Initial commit

---
+++ src/main/java/com/prigozhaeva/aerocalculations/AeroCalculationsApplication.java
@@ -0,0 +1,13 @@
+package com.prigozhaeva.aerocalculations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroCalculationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AeroCalculationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prigozhaeva/aerocalculations/AeroCalculationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prigozhaeva.aerocalculations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroCalculationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
