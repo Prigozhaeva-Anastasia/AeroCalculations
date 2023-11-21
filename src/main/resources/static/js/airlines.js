87f02c1aa9bebb124109dca8afd48d0dbcbc68b8
Prigozhaeva-Anastasia <nastaprigozaeva20@gmail.com>
2023-11-21 19:15:19 +0300

    Edit import button on airline view page

---
+++ src/main/resources/static/js/airlines.js
@@ -0,0 +1,15 @@
+function getPathOfImportFile() {
+    let chooseFile = document.getElementById('importBtn');
+    let pathOfImportFile = "D:/diploma/import/" + chooseFile.files[0].name;
+    $.ajax({
+        url: "/api/airlines/import",
+        type: 'POST',
+        data: {"path": pathOfImportFile},
+        success: () => {
+            location.reload()
+        },
+        error: (err) => {
+            alert(err);
+        }
+    });
+}
