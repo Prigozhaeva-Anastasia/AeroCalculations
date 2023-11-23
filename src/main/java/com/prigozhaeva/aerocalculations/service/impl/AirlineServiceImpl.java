d45b2ab9db025b5223eb98fb0754858c8a080a33
Prigozhaeva-Anastasia <nastaprigozaeva20@gmail.com>
2023-11-23 12:04:11 +0300

    Delete class StaxParser and transfer parse methods into services

--- src/main/java/com/prigozhaeva/aerocalculations/service/impl/AirlineServiceImpl.java
+++ src/main/java/com/prigozhaeva/aerocalculations/service/impl/AirlineServiceImpl.java
@@ -8,6 +8,13 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.XMLEvent;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,7 +28,7 @@ public class AirlineServiceImpl implements AirlineService {
 
     @Override
     public void importAirlines(String path) {
-        List<Airline> airlines = STAXParser.parse(path);
+        List<Airline> airlines = parse(path);
         if (airlines != null && airlines.size() != 0) {
             airlineRepository.saveAll(airlines);
         }
@@ -55,5 +62,71 @@ public class AirlineServiceImpl implements AirlineService {
         return airlineRepository.findAll();
     }
 
-
+    public static List<Airline> parse(String path) {
+        List<Airline> airlines;
+        try {
+            XMLStreamReader reader = XMLInputFactory.newInstance()
+                    .createXMLStreamReader(new FileInputStream(path));
+            airlines = new ArrayList<>();
+            Airline airline = new Airline();
+            while (reader.hasNext()) {
+                reader.next();
+                if (reader.getEventType() == XMLEvent.START_ELEMENT) {
+                    switch (reader.getName().getLocalPart()) {
+                        case "corp":
+                            airline = new Airline();
+                            break;
+                        case "id":
+                            reader.next();
+                            if (reader.getEventType() == XMLEvent.CHARACTERS) {
+                                airline.setId(Long.valueOf(reader.getText()));
+                            }
+                        case "name":
+                            reader.next();
+                            if (reader.getEventType() == XMLEvent.CHARACTERS) {
+                                airline.setPayerName(reader.getText());
+                            }
+                            break;
+                        case "name_short":
+                            reader.next();
+                            if (reader.getEventType() == XMLEvent.CHARACTERS) {
+                                airline.setName(reader.getText());
+                            }
+                            break;
+                        case "country_code":
+                            reader.next();
+                            if (reader.getEventType() == XMLEvent.CHARACTERS) {
+                                airline.setCountryCode(reader.getText());
+                            }
+                            break;
+                        case "TEL":
+                            reader.next();
+                            if (reader.getEventType() == XMLEvent.CHARACTERS) {
+                                airline.setPhone_number(reader.getText());
+                            }
+                            break;
+                        case "ADR":
+                            reader.next();
+                            if (reader.getEventType() == XMLEvent.CHARACTERS) {
+                                airline.setAddress(reader.getText());
+                            }
+                            break;
+                    }
+                }
+                if (reader.getEventType() == XMLEvent.END_ELEMENT) {
+                    Airline fAirline = airline;
+                    boolean elementExists = airlines.stream()
+                            .anyMatch(obj -> obj.getId().equals(fAirline.getId()));
+                    if (!elementExists) {
+                        airlines.add(fAirline);
+                    }
+                }
+            }
+        } catch (XMLStreamException e) {
+            throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return airlines;
+    }
 }
