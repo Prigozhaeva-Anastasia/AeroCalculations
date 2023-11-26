469418c93d9327c2a399a46f3a87105d2268244e
Prigozhaeva-Anastasia <nastaprigozaeva20@gmail.com>
2023-11-26 16:08:02 +0300

    Add flight view page

---
+++ src/main/resources/static/js/flights.js
@@ -0,0 +1,15 @@
+function getPathOfImportFile() {
+    let chooseFile = document.getElementById('importBtn');
+    let pathOfImportFile = "D:/diploma/import/" + chooseFile.files[0].name;
+    $.ajax({
+        url: "/api/flights/import",
+        type: 'POST',
+        data: {"path": pathOfImportFile},
+        success: () => {
+            location.reload()
+        },
+        error: () => {
+            alert("При импорте произошла ошибка, проверьте xml-файл!");
+        }
+    });
+}
