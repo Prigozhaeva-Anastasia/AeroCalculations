199de64263d60029a97d5bf67f736e387d21c2ef
Prigozhaeva-Anastasia <nastaprigozaeva20@gmail.com>
2023-11-29 18:56:17 +0300

    Add filter to the flight's view page

--- src/main/resources/static/js/flight.js
+++ src/main/resources/static/js/flight.js
@@ -9,6 +9,7 @@ function clearLocalStorage() {
 function getPathOfImportFile() {
     let chooseFile = document.getElementById('importBtn');
     let pathOfImportFile = "D:/diploma/import/" + chooseFile.files[0].name;
+
     $.ajax({
         url: "/api/flights/import",
         type: 'POST',
@@ -19,5 +20,14 @@ function getPathOfImportFile() {
         error: () => {
             alert("При импорте произошла ошибка, проверьте xml-файл!");
         }
+    });
+}
+window.onload = function () {
+    let filterForFlights = document.getElementById('filterForFlights');
+    let formFilterForFlights = document.getElementById('formFilterForFlights');
+    let formBtns = document.getElementById('formBtns');
+    filterForFlights.addEventListener('click', function (event) {
+        formFilterForFlights.style.display = 'block';
+        formBtns.style.left = '-20px';
     });
 }
