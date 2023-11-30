eda8e393f7b5cc5484c5f1345537432a65e7d6ca
Prigozhaeva-Anastasia <nastaprigozaeva20@gmail.com>
2023-11-30 12:59:34 +0300

    Add import link in the service view page

---
+++ src/main/resources/static/js/services.js
@@ -0,0 +1,16 @@
+function getPathOfImportFile() {
+    let chooseFile = document.getElementById('importBtn');
+    let pathOfImportFile = "D:/diploma/import/" + chooseFile.files[0].name;
+
+    $.ajax({
+        url: "/api/services/import",
+        type: 'POST',
+        data: {"path": pathOfImportFile},
+        success: () => {
+            location.reload()
+        },
+        error: () => {
+            alert("При импорте произошла ошибка, проверьте xml-файл!");
+        }
+    });
+}
