960fde73fbe8573a269765ddf9fe3e75b4ce7320
Prigozhaeva-Anastasia <nastaprigozaeva20@gmail.com>
2024-01-24 03:17:53 +0300

    Edit function change payment status

--- src/main/resources/static/js/invoices.js
+++ src/main/resources/static/js/invoices.js
@@ -1,7 +1,9 @@
 window.onload = function () {
+    let arrPaymentStateHid = document.querySelectorAll(".paymentStateHid");
     for (let i = 0; i < arrPaymentStateHid.length; i++) {
         let formPaymentState = document.getElementById("formPaymentState_" + i);
         let state = document.getElementById("paymentState_" + i);
+        state.value = arrPaymentStateHid[i].defaultValue;
         if (state.value == "Оплачен") {
             state.style.backgroundColor = "#35D05F";
             state.style.color = "white";
